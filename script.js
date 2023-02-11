4f7bd789bd763028efebbeea6e29ba9189d879b7
nikhilg2809 <66797642+nikhilg2809@users.noreply.github.com>
2023-02-11 23:15:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let arr = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
+  { id: 3, name: "karen", age: "19", marks: 35 }
 ];
 
 function PrintStudentswithMap() {
@@ -14,9 +14,7 @@ function PrintStudentsbyForEach() {
   //Write your code here , just console.log
   arr.forEach(arr => {
   if (arr.marks > 50) {
-    console.log(arr);
-  }
-});
+    console.log(arr);}});
 }
 
 function addData() {
@@ -28,8 +26,17 @@ function addData() {
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  arr = arr.filter(arr => {
+  return arr.marks >= 50;});
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr2 = [
+  { id: 5, name: "klaus", age: "22", marks: 90 },
+  { id: 6, name: "stephen", age: "19", marks: 75 },
+  { id: 7, name: "demon", age: "20", marks: 70 }];
+  let allStudents = arr.concat(arr2);
+  console.log(allStudents);
 }
